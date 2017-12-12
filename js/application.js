be865b970a625bc5cb513270fb903e5a7496f2fb
hombinwong <945372160@qq.com>
2017-12-12 16:01:47 +0800

    Site updated: 2017-12-12 16:01:44

--- js/application.js
+++ js/application.js
@@ -1,3 +1,62 @@
-// build time:Mon Dec 04 2017 23:46:43 GMT+0800 (中国标准时间)
-$(function(){$('[data-toggle="tooltip"]').tooltip();if(typeof $.fn.slimScroll!="undefined"){$(".sidebar .slimContent").slimScroll({height:"auto",color:"rgba(0,0,0,0.2)",size:"3px"})}$(".geopattern").each(function(){$(this).geopattern($(this).data("pattern-id"))});var t=$("#nav-main").okayNav({swipe_enabled:false});$(".donate-box").on("click",".pay_item",function(){var t=$(this).attr("data-id");var a=$(this).attr("data-src")?$(this).attr("data-src"):"assets/images/donate/"+t+"img.png";var e=t=="alipay"?"支付宝":"微信";$(this).addClass("checked").siblings(".pay_item").removeClass("checked");$(".donate-payimg img").attr("src",a);$("#donate-pay_txt").text(e)});$("[data-stick-bottom]").keepInView({fixed:false,parentClass:"has-sticky",customClass:"sticky",trigger:"bottom",zindex:42,edgeOffset:0});$("[data-stick-top]").keepInView({fixed:true,parentClass:"has-sticky",customClass:"sticky",trigger:"top",zindex:42,edgeOffset:0})});
-//rebuild by neat 
+$(function() {
+  // bootstrap tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // slimscroll
+  if (typeof $.fn.slimScroll != 'undefined') {
+    $(".sidebar .slimContent").slimScroll({
+      height: "auto",
+      color: "rgba(0,0,0,0.2)",
+      size: "3px",
+      // allowPageScroll: true
+    });
+  }
+
+  // geopattern 背景生成
+  $(".geopattern").each(function() {
+    $(this).geopattern($(this).data('pattern-id'));
+  });
+
+  // okayNav
+  var navigation = $('#nav-main').okayNav({
+    swipe_enabled: false, // If true, you'll be able to swipe left/right to open the navigation
+  });
+
+  // donate
+  $('.donate-box').on('click', '.pay_item', function() {
+    var dataid = $(this).attr('data-id');
+    var qrcode = $(this).attr('data-src') ? $(this).attr('data-src') : "assets/images/donate/" + dataid + "img.png";
+    var text = dataid == "alipay" ? "支付宝" : "微信";
+    $(this).addClass('checked').siblings('.pay_item').removeClass('checked');
+    $(".donate-payimg img").attr("src", qrcode);
+    $("#donate-pay_txt").text(text);
+  });
+
+  // modal居中
+  // $('.modal').on('shown.bs.modal', function(e) {
+  //   $(this).show();
+  //   var modalDialog = $(this).find(".modal-dialog");
+  //    // Applying the top margin on modal dialog to align it vertically center 
+  //   modalDialog.css("margin-top", Math.max(0, ($(window).height() - modalDialog.height()) / 2));
+  // });
+
+  // sticky
+  $('[data-stick-bottom]').keepInView({
+    fixed: false,
+    parentClass: "has-sticky",
+    customClass: "sticky",
+    trigger: 'bottom',
+    zindex: 42,
+    edgeOffset: 0
+  });
+  $('[data-stick-top]').keepInView({
+    fixed: true,
+    parentClass: "has-sticky",
+    customClass: "sticky",
+    trigger: 'top',
+    zindex: 42,
+    edgeOffset: 0
+  });
+});
+
+//# sourceMappingURL=application.js.map
